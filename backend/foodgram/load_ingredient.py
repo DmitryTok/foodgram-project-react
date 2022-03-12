fee21be3749e2979adbdbdf5ace349b84e917059
Dmitry <dmitry_tok@MacBook-Pro-Dmitry.local>
2022-03-12 10:41:05 +0200

    fads

--- backend/foodgram/load_ingredient.py
+++ backend/foodgram/load_ingredient.py
@@ -14,14 +14,12 @@ class Command(BaseCommand):
     help = "loading ingredients from data in json"
 
     def add_arguments(self, parser):
-        parser.add_argument(
-            "filename", default="ingredients.json", nargs="?", type=str)
+        parser.add_argument("filename", default="ingredients.json", nargs="?", type=str)
 
     def handle(self, *args, **options):
         try:
             with open(
-                os.path.join(
-                    DATA_ROOT, options["filename"]), "r", encoding="utf-8"
+                os.path.join(DATA_ROOT, options["filename"]), "r", encoding="utf-8"
             ) as f:
                 data = json.load(f)
                 for ingredient in data:
