d0148b0219f5ca6bb498c410dc425233c9831b18
Dmitry <dmitry_tok@MacBook-Pro-Dmitry.local>
2022-03-19 10:59:23 +0200

    finaly

--- backend/api/management/commands/ingredients.py
+++ backend/api/management/commands/ingredients.py
@@ -7,29 +7,35 @@ from django.db.utils import IntegrityError
 
 from api.models import Ingredient
 
-DATA_ROOT = os.path.join(settings.BASE_DIR, 'data')
+DATA_ROOT = os.path.join(settings.BASE_DIR, "data")
 
 
 class Command(BaseCommand):
-    help = 'loading ingredients from data in json'
+    help = "loading ingredients from data in json"
 
     def add_arguments(self, parser):
-        parser.add_argument('filename', default='ingredients.json', nargs='?',
-                            type=str)
+        parser.add_argument(
+            "filename", default="ingredients.json", nargs="?", type=str
+        )
 
     def handle(self, *args, **options):
         try:
-            with open(os.path.join(DATA_ROOT, options['filename']), 'r',
-                      encoding='utf-8') as f:
+            with open(
+                os.path.join(
+                    DATA_ROOT, options["filename"]), "r", encoding="utf-8"
+            ) as f:
                 data = json.load(f)
                 for ingredient in data:
                     try:
-                        Ingredient.objects.create(name=ingredient["name"],
-                                                  measurement_unit=ingredient[
-                                                      "measurement_unit"])
+                        Ingredient.objects.create(
+                            name=ingredient["name"],
+                            measurement_unit=ingredient["measurement_unit"],
+                        )
                     except IntegrityError:
-                        print(f'Ingredient {ingredient["name"]} '
-                              f'{ingredient["measurement_unit"]} '
-                              f'already in data base')
+                        print(
+                            f'Ingredient {ingredient["name"]} '
+                            f'{ingredient["measurement_unit"]} '
+                            f"already in data base"
+                        )
         except FileNotFoundError:
-            raise CommandError('the file is missing data')
+            raise CommandError("the file is missing data")
