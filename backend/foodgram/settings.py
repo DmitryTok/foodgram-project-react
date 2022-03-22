9525d924ea48ca038e415759bec691da7292791f
Dmitry <dmitry_tok@MacBook-Pro-Dmitry.local>
2022-03-22 22:41:36 +0200

    second

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -95,7 +95,7 @@ REST_FRAMEWORK = {
         "rest_framework.permissions.IsAuthenticatedOrReadOnly",
     ],
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',,
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     "PAGE_SIZE": 5,
 }
 
